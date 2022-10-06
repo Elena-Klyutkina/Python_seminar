012a77e960d45717e0692879458e92d5fd795cc6
Klyutkina <Elena.Klyutkina@x5.ru>
2022-10-06 21:30:41 +0300

    04_1

---
+++ sem04/homework_04_1.py
@@ -0,0 +1,6 @@
+# Вычислить число c заданной точностью d
+# Пример:
+# - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+from decimal import Decimal
+print(Decimal(3.1415926535).quantize(Decimal("1.00")))
